1167be45c014877fa2aee2bb2319d075682ee104
Enrique Galvez <quique66200@hotmail.com>
2023-03-30 23:02:45 +0200

    not working build system

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../deps/stb_image.h"
+
+#include "imgui.h"
+#include "imgui_impl_glut.h"
+#include "imgui_impl_opengl2.h"
+#define GL_SILENCE_DEPRECATION
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+#include "imgui.h"
+
+#include "utils.cpp"
+
+int main(){
+	// Load image in OpenGL
+	int my_image_width = 0;
+	int my_image_height = 0;
+	GLuint my_image_texture = 0;
+	bool ret = LoadTextureFromFile("../images/castle_orig.jpg", &my_image_texture, &my_image_width, &my_image_height);
+	IM_ASSERT(ret);
+
+	// Open image in ImGUI
+	ImGui::Begin("OpenGL Texture Text");
+	ImGui::Text("pointer = %p", my_image_texture);
+	ImGui::Text("size = %d x %d", my_image_width, my_image_height);
+	ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+	ImGui::End();
+}
